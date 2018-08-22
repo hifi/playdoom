035e364bbba8993587c66496ed4231ff53666ac6
Toni Spets <toni.spets@iki.fi>
2018-08-22 11:43:18 +0300

    Convert most of doom.js into async world

--- docs/js/doom.js
+++ docs/js/doom.js
@@ -1,89 +1,105 @@
-// uh oh, I'm no JS developer
-var Doom = function(canvas, logger) {
-    zip.workerScriptsPath = "js/zip/";
+// uh oh, I'm no JS developer (but I'm getting better)
+class Doom {
+    constructor(canvas, logger) {
+        this.canvas = canvas;
+        this.logger = logger;
+        zip.workerScriptsPath = "js/zip/";
+        this.init();
+    }
 
-    var loadFile = function(path, cb) {
-        var r = new XMLHttpRequest();
-        r.responseType = 'arraybuffer';
-        r.open('GET', path);
-        r.addEventListener('load', function() {
-            cb(r.response);
+    async init() {
+        this.log('doom.js init');
+        this.log('Loading doom19s.zip...');
+
+        var file = await Doom.loadFile('doom19s.zip');
+
+        this.log('Extracting doom19s.zip...');
+
+        var files = await this.unzip(file, ['DOOMS_19.1', 'DOOMS_19.2']);
+
+        this.log('Combining DOOMS_19.1 and DOOMS_19.2 as DOOM_19.zip...');
+        var innerZip = await Doom.joinBlobsAsArrayBuffer([ files['DOOMS_19.1'], files['DOOMS_19.2'] ]);
+
+        this.log('Extracting DOOM1.WAD from DOOMS_19.zip...');
+        var innerFiles = await this.unzip(innerZip, ['DOOM1.WAD']);
+
+        var iwad = await Doom.joinBlobsAsArrayBuffer([ innerFiles['DOOM1.WAD'] ]);
+        this.run(iwad);
+    }
+
+    static loadFile(path, cb) {
+        return new Promise(resolve => {
+            var r = new XMLHttpRequest();
+            r.responseType = 'arraybuffer';
+            r.open('GET', path);
+            r.addEventListener('load', function() {
+                resolve(r.response);
+            });
+            r.send();
         });
-        r.send();
-    };
+    }
 
-    var log = function(text) {
-        logger.append(text + "\n");
-        logger.scrollTop = logger.scrollHeight;
-    };
+    log(text) {
+        this.logger.append(text + "\n");
+        this.logger.scrollTop = this.logger.scrollHeight;
+    }
 
-    var unzip = function(data, files, callback) {
-        var out = [];
+    unzip(data, files) {
+        var d = this;
+        return new Promise(resolve => {
+            var out = [];
 
-        zip.createReader(new zip.BlobReader(new Blob([data], { type: 'application/zip' })), function(reader) {
-            reader.getEntries(function(entries) {
-                for (var i in entries) {
-                    var e = entries[i];
-                    if (files.indexOf(e.filename) > -1) {
-                        (function(e) {
-                            log('... ' + e.filename);
+            zip.createReader(new zip.BlobReader(new Blob([data], { type: 'application/zip' })), function(reader) {
+                reader.getEntries(function(entries) {
+                    for (var e of entries) {
+                        if (files.indexOf(e.filename) == -1)
+                            continue;
+
+                        (function(e,d) {
+                            d.log('... ' + e.filename);
                             e.getData(new zip.BlobWriter(), function(edata) {
                                 out[e.filename] = edata;
                                 if (Object.keys(out).length == files.length) {
-                                    callback(out);
+                                    resolve(out);
                                 }
                             });
-                        })(e);
+                        })(e,d);
                     }
-                }
+                });
             });
         });
-    };
+    }
 
-    var joinBlobsAsArrayBuffer = function(blobs, callback) {
-        var out = [];
-        var train = [];
+    static async joinBlobsAsArrayBuffer(blobs) {
+        const totalSize = blobs.reduce((a,b) => a + b.size, 0);
 
-        train[blobs.length] = function() {
-            var totalSize = 0;
-            for (var i in out) {
-                totalSize += out[i].byteLength;
-            }
+        var out = new Uint8Array(totalSize);
+        var pos = 0;
 
-            var joined = new Uint8Array(totalSize);
-
-            var pos = 0;
-            for (var i in out) {
-                joined.set(new Uint8Array(out[i]), pos);
-                pos += out[i].byteLength;
-            }
-
-            callback(joined.buffer);
-        };
-
-        for (var i = blobs.length - 1; i >= 0; i--) {
-            train[i] = (function(b, next) {
-                return function() {
-                    var r = new FileReader();
-                    r.onload = function(e) {
-                        out.push(e.target.result);
-                        next();
-                    };
-                    r.readAsArrayBuffer(b);
+        for (var blob of blobs) {
+            var ab = await new Promise(resolve => {
+                var r = new FileReader();
+                r.onload = function(e) {
+                    resolve(e.target.result);
                 };
-            })(blobs[i], train[i + 1]);
+                r.readAsArrayBuffer(blob);
+            });
+
+            out.set(new Uint8Array(ab), pos);
+            pos += ab.byteLength;
         }
 
-        train[0]();
-    };
+        return out;
+    }
 
-    var startChocolateDoom = function(iwad) {
-        log('Preload completed, launching Chocolate-Doom');
-        log('');
+    run(iwad) {
+        this.log('Preload completed, launching Chocolate-Doom');
+        this.log('');
 
+        var d = this;
         window.Module = {
-            print: log,
-            printErr: log,
+            print: function(s) { d.log(s); },
+            printErr: function(s) { d.log(s); },
             canvas: document.getElementById('game'),
             arguments: [ '-iwad',  '/doom1.wad' ],
             quit: function() {
@@ -99,10 +115,9 @@ var Doom = function(canvas, logger) {
             FS.chdir('/data');
 
             FS.writeFile('/doom1.wad', new Uint8Array(iwad));
-            console.log(runDependencies);
             runDependencies++;
 
-            log('Going to sync config from IndexedDB...');
+            d.log('Going to sync config from IndexedDB...');
             FS.syncfs(true, function(e) {
                 try {
                     FS.lookupPath('/data/default.cfg');
@@ -124,7 +139,7 @@ var Doom = function(canvas, logger) {
                     ].join("\n"));
                 }
 
-                log('Sync from IndexedDB done, starting app.');
+                d.log('Sync from IndexedDB done, starting app.');
                 runDependencies--;
                 run();
             });
@@ -133,24 +148,5 @@ var Doom = function(canvas, logger) {
         var s = document.createElement('script');
         s.src = 'js/chocolate-doom.js';
         document.getElementsByTagName('body')[0].append(s);
-    };
-
-    log('doom.js init');
-
-    log('Loading doom19s.zip...');
-
-    loadFile('doom19s.zip', function(file) {
-        log('Extracting doom19s.zip...');
-
-        unzip(file, ['DOOMS_19.1', 'DOOMS_19.2'], function(files) {
-            log('Combining DOOMS_19.1 and DOOMS_19.2 as DOOM_19.zip...');
-
-            joinBlobsAsArrayBuffer([ files['DOOMS_19.1'], files['DOOMS_19.2'] ], function(innerZip) {
-                log('Extracting DOOM1.WAD from DOOMS_19.zip...');
-                unzip(innerZip, ['DOOM1.WAD'], function(innerFiles) {
-                    joinBlobsAsArrayBuffer([ innerFiles['DOOM1.WAD'] ], startChocolateDoom);
-                });
-            });
-        });
-    });
+    }
 };
